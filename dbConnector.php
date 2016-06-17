dcca3d5fde8d838e6cf7c5c14f4100b450c706d7
Ryan Watt <ryanwatt@Ryans-MacBook-Pro-2.local>
2016-06-17 14:39:19 -0600

    dbConnector info

--- dbConnector.php
+++ dbConnector.php
@@ -6,30 +6,24 @@ function loadDatabase()
 
   $dbHost = "";
   $dbPort = "";
-  $dbUser = "";
-  $dbPassword = "";
+  $dbUser = "adminavGGRVZ";
+  $dbPassword = "uan79BAVksBA";
 
-     $dbName = "testdb";
+     $dbName = "basketballproject";
 
      $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
 
-     if ($openShiftVar === null || $openShiftVar == "")
-     {
-          // Not in the openshift environment
-          //echo "Using local credentials: ";
-          require("setLocalDatabaseCredentials.php");
-     }
-     else
-     {
-          // In the openshift environment
-          //echo "Using openshift credentials: ";
-
+    //  if ($openShiftVar === null || $openShiftVar == "")
+    //  {
+    //       require("setLocalDatabaseCredentials.php");
+    //  }
+    //  else
+    //  {
           $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
           $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
           $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
           $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-     }
-     //echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
+     //}
 
      $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
